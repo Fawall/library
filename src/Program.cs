8a9df66c927168c981f469c20d18a3d14612fc0b
Fabio Lopes <fabiolopes7844@gmail.com>
2023-01-18 12:05:38 -0300

    Done the presentation of library

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Welcome. Type what you want to do");
+
+System.Console.WriteLine("##################################\n"+
+                          "#                               #\n"+
+                          "#  1 - Insert a new book        #\n"+
+                          "#  2 - Edit details of book     #\n"+
+                          "#  3 - Delete book              #\n"+
+                          "#  4 - Get all books            #\n"+
+                          "#  5 - Search one specify book  #\n"+
+                          "#                               #\n"+
+                          "#################################");
