1449619dd00e5d549f0216234228342cbe0186b9
Fabio Lopes <fabiolopes7844@gmail.com>
2023-01-19 11:11:56 -0300

    Added prop Id in Book Class

--- src/Book.cs
+++ src/Book.cs
@@ -2,14 +2,17 @@
 
 class Book {
 
+    private static int _nextId {get; set;}
+    public int Id {get; set;}
     public string Title { get; set;}
     public string Description { get; set;}
     public string Genre { get; set; }
-    public string Pages {get; set;}
+    public int Pages {get; set;}
 
 
-    public Book(string title, string description, string genre, string pages)
+    public Book(string title, string description, string genre, int pages)
     {
+        Id = _nextId;   
         Title = title;
         Description = description;
         Genre = genre;
